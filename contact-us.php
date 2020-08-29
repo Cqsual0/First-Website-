d3ac1eb6d5b6172972a773b42127bd8d1075d1c0
Cqsual0 <57657245+Cqsual0@users.noreply.github.com>
2020-08-29 18:36:35 +0930

    Contact-us

---
+++ contact-us.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- LINKS -->
+  <meta name="viewport" content="width=device-width, initial-scale=.7">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:700&display=swap" rel="stylesheet">
+  <link href="./css/shop.css" rel="stylesheet" type="text/css">
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/6ee8fbe20c.js" crossorigin="anonymous"></script>
+  <!-- FAVICON -->
+  <link rel="apple-touch-icon" sizes="57x57" href="image/favicon_io//apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="image/favicon_io//apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="image/favicon_io//apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="image/favicon_io//apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="image/favicon_io//apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="image/favicon_io//apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="image/favicon_io//apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="image/favicon_io//apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="image/favicon_io//apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="image/favicon_io//android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="image/favicon_io//favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="image/favicon_io//favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="image/favicon_io//favicon-16x16.png">
+  <link rel="manifest" href="/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <!-- FAVICON -->
+
+  <!-- Load Stripe.js on your website. -->
+<script src="https://js.stripe.com/v3"></script>
+
+  <!-- META CHARSET -->
+  <meta charset="utf-8">
+  <!-- META VIEWPORT -->
+  <meta name="viewport" content="width=device-width, initial-scale=0.6">
+  <!-- META EDGE -->
+  <meta http-equiv="X_UA-Compatible" content="ie=edge">
+
+  <!-- META CONTENT -->
+  <title>Skincoin | Contact Us</title>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160010738-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-160010738-1');
+</script>
+
+<meta name="google-site-verification" content="UrjRiNj_DbW3dKUaiVEkmkF8LzAsiTx9zPElzncbHu4" />
+  <!-- End-Links -->
+</head>
+<body>
+  <!-- START -->
+
+  <!-- Navigation-Bar -->
+  <div class="nav">
+    <ul id="navUL">
+      <a href="index.html">
+        <li><img src="image/logo.png"></li>
+      </a>
+      <a href="home">
+        <li>Home</li>
+      </a>
+      <a href="shop">
+        <li>Accounts</li>
+      </a>
+      <a href="contact-us">
+        <li>Contact us</li>
+      </a>
+    </ul>
+  </div>
+
+  <p>SEND E-MAIL</p>
+  <form class="contact-form" action="contactform.php" method="post">
+  <input type="text" name="name" placeholder="Full name">
+  <input type="text" name="mail" placeholder="Subject">
+  <input type="text" name="Subject" placeholder="Subject">
+  <textarea name="message" placeholder="Message"></textarea>
+  <button type="submit" name="submit">SEND MAIL</button>
+  </form>
+
+    <!-- Footer -->
+    <div class="fourth">
+      <ul>
+        <a href="index.html">
+          <li id="logo"><img src="image/logo.png"></li>
+        </a>
+        <li>
+            <h4> © COPYRIGHT 2020 SkinCoin.me
+              is in no way affiliated with Mojang, AB. Nor should it be considered a company or trademark endorsed by Mojang, AB. <br>
+          </h4>
+        </li>
+        <li>
+          <h4>Links</h4><br><a href="home">Home</a><br><a href="shop">Accounts</a><br><a href="contact-us">Contact-Us</a><br>
+        </li></a>
+        <li>
+          <h4>Company</h4><br><a href="tos">Terms of service</a><br><a href="pp">Privacy Policy</a><br><a href="about">About us</a><br>
+        </li></a>
+        <li>
+          <h4>Login</h4><br><a href="http://web.continuumsolutions.online:2083/">cPanel</a><br><br><br>
+          <li id="discord"><img src="image/discord.webp"><a href="https://discord.gg/ae2dGp4" target="_blank"></li>
+        </li>
+      </ul>
+    </div>
+    <!-- Footer-End -->
+
+
+</body>
+</script>
+
+</html>
