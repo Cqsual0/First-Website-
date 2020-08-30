8de6b3ce73d6247ed64b7950d7716aa79ebb8ecc
Cqsual0 <57657245+Cqsual0@users.noreply.github.com>
2020-08-30 15:14:09 +0930

    Back-to-Top

---
+++ javascript/custom.js
@@ -0,0 +1,17 @@
+/* ----------------------------------------------------------- */
+/*  BACK TO TOP
+/* ----------------------------------------------------------- */
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 800) {
+            $("#back-to-top").addClass("show-back-to-top");
+        } else {
+            $("#back-to-top").removeClass("show-back-to-top");
+        }
+    });
+    $("#back-to-top").on("click", function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
